24ba293c06c0a17a1264751c0ffc0914d90b8818
brucemaxx <silvabruce777@gmail.com>
2025-07-11 16:00:16 -0300

    Create dashboard_router.py

---
+++ app/routes/dashboard_router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from app.database.db import SessionLocal
+from sqlalchemy.orm import Session
+from app.models.alerta import Alerta
+
+
+
+
+router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
+
+# Dependência para obter o banco
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+
+@router.get("/dashboard")
+def exibir_dashboard(request: Request, db: Session = Depends(get_db)):
+    alertas = db.query(Alerta),order_by(Alerta.data_envio.desc()).all()
+    return templates.TempalateResponse("dasboard.html", {"request": request, "alertas": alertas})
