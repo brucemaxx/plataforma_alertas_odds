9f0067c9f3ea9ca687fb11332a69d153dea7ec6d
brucemaxx <silvabruce777@gmail.com>
2025-07-18 16:51:16 -0300

    Update proteção
    Criando proteção para os acessos.
    Dependências protegidas ao autenticar, em utils e em routes

--- app/routes/alerta_router.py
+++ app/routes/alerta_router.py
@@ -6,6 +6,8 @@ from app.database.conexao import SessionLocal
 from app.models.alerta import Alerta
 from app.schemas.alerta_schema import AlertaSchema
 from typing import List
+from app.utils.security import get_current_user
+from app.models.user import User
 
 # Retorna todos os alertas em formato JSON
 router = APIRouter()
@@ -22,3 +24,7 @@ def get_db():
 def listar_alertas(db: Session = Depends(get_db)):
     alertas = db.query(Alerta).all()
     return alertas
+
+@router.get("/protegida")
+def rota_segura(usuario: User = Depends(get_current_user)):
+    return {"msg": f"Olá,{usuario.username}. Você está autenticado!"}
--- app/utils/security.py
+++ app/utils/security.py
@@ -21,3 +21,36 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+# Dependência de auteticação
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from database.conexao import get_db
+from models.user import User
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db))-> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Credenciais inválidas",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credentials_exception
+        
+        
+    return user
