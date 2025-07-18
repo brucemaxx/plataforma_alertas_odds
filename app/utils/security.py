fec9ab643549c43ff24a63d3aa63b0fe856b8adf
brucemaxx <silvabruce777@gmail.com>
2025-07-18 11:13:42 -0300

    Update - security

---
+++ app/utils/security.py
@@ -0,0 +1,24 @@
+
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+
+# Configurações
+SECRET_KEY = "sua_chave_super_secreta"
+ALGORITHM = "HS256"
+ACESS+TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_acess_token(data: dict, expires_delta, timedelta = None):
+    to_encode - data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACESS_TOKEN_EXPIRE_MINUTES))
+    to-encode.upadate({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
