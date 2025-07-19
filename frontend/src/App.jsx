c77a09d3e520f36fe64a99c48bf12dde01b67080
brucemaxx <silvabruce777@gmail.com>
2025-07-18 22:16:21 -0300

    Update src/App.jsx
    Criado o token de login para autenticação do usuário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="bg-gray-50 min-h-screen">
       <Dashboard />
+    </div>
+  );
+}
+
+// Controlando o estado do token de autenticação.
+// Se o token estiver presente, renderiza o Dashboard, caso contrário, renderiza o Login.
+function App() {
+  const [token, setToken] = userState(localStorage.getItem("token"));
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
+  return (
+    <div>
+      {token ? (
+        < >
+        <button onClick={handleLogout} className="m-4 bg-red-500 text-white p-2 rounded hover:bg-red-600">
+          Sair
+        </button>
+        <Dashboard token={token} />
+        </>
+      ) : (
+        <Login setToken={setToken} />
+      )}
     </div>
   );
 }
